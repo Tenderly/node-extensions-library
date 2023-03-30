df3867ed288c8d1cd9030154d805703c40995c6b
dzimiks <vana997@gmail.com>
2023-03-30 22:59:15 +0200

    feat: add simulate tx node extension

--- simulate-send-transaction/actions/extension.ts
+++ simulate-send-transaction/actions/extension.ts
@@ -1,32 +1,40 @@
-import {ActionFn, Context, Event, Network} from '@tenderly/actions';
-
-import {ethers} from 'ethers';
+import { ActionFn, Context, Event, Network, WebhookEvent } from '@tenderly/actions';
+import { ethers } from 'ethers';
 
 export const sendSimulateRawTransaction: ActionFn = async (context: Context, event: Event) => {
-	// Setting a variable that will store the Web3 Gateway RPC URL and secret key
-	const defaultGatewayURL = context.gateways.getGateway(Network.MAINNET);
+  // Casting the event to a WebhookEvent
+  const webhookEvent: WebhookEvent = event as WebhookEvent;
 
-	// Using the Ethere.js provider class to call the RPC URL
-	const provider = new ethers.JsonRpcProvider(defaultGatewayURL);
+  // Setting a variable that will store the Web3 Gateway RPC URL and secret key
+  const defaultGatewayURL = context.gateways.getGateway(Network.MAINNET);
 
-	// Decode raw transaction
-	const tx = ethers.Transaction.from(event)
+  // Using the Ethers.js provider class to call the RPC URL
+  const provider = new ethers.providers.JsonRpcProvider(defaultGatewayURL);
 
-	// Simulate transaction to get execution results
-	const simResponse = await provider.send("tenderly_simulateTransaction", [{
-		"from": tx.from,
-		"to": tx.to,
-		"gas": tx.gasLimit,
-		"gasPrice": tx.gasPrice,
-		"value": tx.value,
-		"data": tx.data,
-	}])
+  // Decode raw transaction
+  const tx = {
+    ...webhookEvent.payload,
+    data: webhookEvent.payload.data || webhookEvent.payload.input,
+  };
 
-	// If simulation fails, return error
-	if (simResponse.status == false) {
-		return null
-	}
+  // Simulate transaction to get execution results
+  const simResponse = await provider.send('tenderly_simulateTransaction', [
+    {
+      ...tx,
+    },
+    'latest',
+  ]);
+  console.log({ simResponse });
 
-	// Send transaction to the network
-	return provider.send('eth_sendRawTransaction', event)
-}
+  // If simulation fails, return error
+  if (simResponse.status == false) {
+    return Promise.reject({
+      error: 'Simulation failed',
+      simResponse,
+    });
+  }
+
+  return simResponse;
+  // Send transaction to the network
+  // return provider.send('eth_sendRawTransaction', tx);
+};
